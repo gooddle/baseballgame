d1fc57d4e811d87b5c5e0f065bae3c81f5f9d68e
gooddle <vptl6559@gmail.com>
2024-04-29 15:53:32 +0900

    "메인 함수 및 정답 무작위 생성 스트라이크 볼 카운트 함수 생성 "

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,92 @@
+package org.example
+
+
+import kotlin.random.Random
+
+fun main(args : Array<String>) {
+    var inresult = game()
+    while (true) {
+        println("게임 시작")
+        val input = readln()!!.toString()
+        when(input.length){
+            3 ->{if(input[0] == input[1] || input[0] == input[2] || input[1] == input[2]){
+                println("동일한 숫자 반복 사용 금지")
+                continue
+            }
+            else if(!input.all { it.isDigit() }){
+                println("숫자 사용")
+                continue
+            }
+            }
+
+            else ->  continue
+
+        }
+        val strikes= countsStrike (input,inresult)
+        val balls= countsBalls(input,inresult)
+
+        println("${strikes}:스트라이크 ,${balls} : 볼")
+        if(strikes == 3){
+            println("게임 종료")
+            break
+        }
+
+
+        }
+
+    }
+
+
+fun game(): String {
+    var answer = (100.. 999).toList()
+    val result =mutableListOf<Int>()
+    for(i in answer.indices){
+        if(answer[i].toString().contains("0")){
+            result.add(i)
+        }else if(answer[0]==answer[1]||answer[0]==answer[2]||answer[1]==answer[2]){
+            result.add(i)
+        }
+    }
+   answer -= result
+   return answer.random().toString()
+   }
+
+fun countsStrike(input: String, answer: String): Int {
+    var strikes = 0
+    for (i in input.indices) {
+        if (input[i] == answer[i]) {
+            strikes++
+        }
+    }
+    return strikes
+}
+
+fun countsBalls(input: String, answer: String): Int {
+    var balls = 0
+    for (i in input.indices) {
+        if (input[i] != answer[i] && input[i] in answer) {
+            balls++
+        }
+    }
+    return balls
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
